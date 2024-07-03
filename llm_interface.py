f7211b33e062f984f4293ee5320b0d5d5a6a7c92
jonasdebeukelaer <jonas.db@live.co.uk>
2024-07-03 17:51:54 +0100

    feat: move rag logic to inside dspy module

---
+++ llm_interface.py
@@ -0,0 +1,54 @@
+import os
+
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+import dspy
+from dspy.retrieve.chromadb_rm import ChromadbRM
+from dspy.evaluate import metrics
+
+CHROMA_COLLECTION_NAME = "books"
+CHROMA_PATH = "chroma"
+os.environ["DSP_CACHEBOOL"] = "false"
+
+
+class RAG(dspy.Module):
+    """
+    This class is a DSPy module which gets a question and returns an answer based
+    on the context it can find in the chroma db.
+    """
+
+    def __init__(self, model_name="gpt-4o", number_of_extracts: int = 3):
+        super().__init__()
+
+        # init LM
+        open_ai_api_key = os.getenv("OPENAI_API_KEY")
+        self.lm = dspy.OpenAI(model_name, api_key=open_ai_api_key, max_tokens=1000)
+
+        # init retriever
+        retriever_model = ChromadbRM(
+            collection_name=CHROMA_COLLECTION_NAME,
+            persist_directory=CHROMA_PATH,
+            embedding_function=OpenAIEmbeddingFunction(api_key=open_ai_api_key),
+        )
+
+        # configure dspy defaults
+        dspy.settings.configure(lm=self.lm, rm=retriever_model)
+
+        # init modules
+        self.retriever = dspy.Retrieve(
+            k=number_of_extracts
+        )  # could define custom retriver which also returns the source document data
+        self.sumariser = dspy.Predict("question, context -> answer")
+
+    def forward(self, question: str) -> dspy.Prediction:
+        """Summarises the information contained in the extracts to answer the question"""
+
+        context = self.retriever(question).passages
+        pred = self.sumariser(question=question, context=context)
+
+        # TODO: bring back (caussing issues during metric checking?)
+        # dspy.Assert(
+        #     not ("Extracts" in pred.answer or "Answer" in pred.answer),
+        #     "The answer should not contain a ref to the context or the word 'Answer' itself",
+        # )
+
+        return pred
--- main.py
+++ main.py
@@ -1,59 +1,13 @@
-import os
 import argparse
 
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-
-from llm import InfoSummariser
-
-CHROMA_PATH = "chroma"
-os.environ["DSP_CACHEBOOL"] = "false"
+from llm_interface import RAG
 
 
 def main(question: str):
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=OpenAIEmbeddings())
-    summariser = InfoSummariser()
-
-    print(f"Question: {question}")
-    extracts = search(db, question)
-
-    if extracts is None or len(extracts) == 0:
-        print("Could not find the answer :(")
-        return
-
-    print("Sending question to answer to LLM...\n")
-    summariser_results = summariser(question, extracts)
-
-    print(f"Answer: {summariser_results.answer}")
-    print(f"Sources used: {summariser_results.source_documents_used}")
-
-
-def search(db, query_text: str, extracts_to_return: int = 3) -> list:
-    print("Searching vector db for relevant extracts...")
-    results = db.similarity_search_with_relevance_scores(query_text, k=extracts_to_return)
-
-    result_set = []
-
-    if len(results) == 0 or results[0][1] < 0.7:
-        print("No good matches found in the provided documents.")
-        return result_set
-
-    print(f"Top {extracts_to_return} relevant extracts:\n")
-
-    i = 0
-    for doc, score in results:
-        i += 1
-        print(f"document: {i} | score: {score} | source: {doc.metadata['source']}\n", doc.page_content, "\n")
-        result_set.append(
-            {
-                "extract": doc.page_content,
-                "extract_relevance_score": score,
-                "source_document": doc.metadata["source"],
-            }
-        )
-
-    print("finished search\n\n")
-    return result_set
+    print(f"Question: {question}\n")
+    rag_results = RAG()(question)
+    print(f"Answer: {rag_results.answer}")
+    print("Sources used: unkown (since now using DSPy built in retriever)")
 
 
 if __name__ == "__main__":
